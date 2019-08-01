f0e0ee805a88a68d6a88a0d9ac12f35caef6711b
mshahzaib1629 <mshahzaib1629@gmail.com>
2019-08-01 09:51:36 +0500

    Initial Commit
    Basic Ping Operation Implemented

---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ping_Checking_System
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            Ping ping = new Ping();
+            PingReply pingReply = ping.Send(textBox1.Text, 10000);
+            richTextBox1.AppendText("Status: " + pingReply.Status.ToString() + "\nAddress: " + pingReply.Address.ToString() + "\nTime: " + pingReply.RoundtripTime.ToString()  + "\nBuffer: " + pingReply.Buffer.ToString());
+
+        }
+    }
+}
