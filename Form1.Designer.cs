060160cb61d5d5b142fe22d0bbf73bc3b9d6f1f1
mshahzaib1629 <mshahzaib1629@gmail.com>
2019-08-08 16:36:03 +0500

    Adding Route Trace with Threads
    Now Async is not implemented, all the response of Trace is although displayed on console but is unable to load on ListBox (UI Element) in real-time

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -44,6 +44,11 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.label7 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -57,9 +62,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(95, 38);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(114, 17);
+            this.label1.Size = new System.Drawing.Size(130, 17);
             this.label1.TabIndex = 1;
             this.label1.Text = "Enter IP Address";
             // 
@@ -76,9 +82,10 @@
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(30, 211);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(72, 17);
+            this.label2.Size = new System.Drawing.Size(80, 17);
             this.label2.TabIndex = 4;
             this.label2.Text = "Response";
             // 
@@ -173,11 +180,62 @@
             this.label7.Size = new System.Drawing.Size(0, 17);
             this.label7.TabIndex = 14;
             // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(681, 246);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(580, 308);
+            this.listBox1.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(681, 210);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(98, 17);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Route Trace";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(678, 38);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(132, 17);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Enter IP to Trace";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(679, 68);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(299, 22);
+            this.textBox6.TabIndex = 18;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1001, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(178, 26);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "TRACE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(698, 581);
+            this.ClientSize = new System.Drawing.Size(1292, 581);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label6);
@@ -217,6 +275,11 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace Ping_Checking_System
 {
@@ -66,7 +67,81 @@ namespace Ping_Checking_System
                 Console.WriteLine(DateTime.Now.TimeOfDay + " \t" + ip + " \t" + pingReply.Status.ToString() + " \t" + pingReply.RoundtripTime.ToString());
                 Console.WriteLine("Successful Pings: " + countSuccess);
             }
-        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // -------------------- Parameters used for Route Trace -----------------------
+            string hostname = textBox6.Text;
+            int timeOut = 1000; // 1000ms or 1 second
+            int max_ttl = 30; //max number of servers allowed to be found
+            const int bufferSize = 32;
+            
+            traceOut(hostname, timeOut, max_ttl, bufferSize);
+           
+        }
+
+        private void traceOut(String hostname, int timeOut, int max_ttl, int bufferSize)
+        {
+            int current_ttl = 0; //used for tracking how many servers have been found.
+            Stopwatch s1 = new Stopwatch();
+            Stopwatch s2 = new Stopwatch();
+
+            byte[] buffer = new byte[bufferSize];
+            new Random().NextBytes(buffer);
+
+            Ping pinger = new Ping();
+            Task.Factory.StartNew(() => this.BeginInvoke((Action)delegate ()
+            {
+                WriteListBox($"Started ICMP Trace route on {hostname}");
+                for (int ttl = 1; ttl <= max_ttl; ttl++)
+                {
+                    current_ttl++;
+                    s1.Start();
+                    s2.Start();
+                    PingOptions options = new PingOptions(ttl, true);
+                    PingReply reply = null;
+                    try
+                    {
+                        reply = pinger.Send(hostname, timeOut, buffer, options);
+                    }
+                    catch
+                    {
+                        WriteListBox("Error");
+                        break; //the rest of the code relies on reply not being null so...
+                    }
+                    if (reply != null) //dont need this but anyway...
+                    {
+                        //the traceroute bits :)
+                        if (reply.Status == IPStatus.TtlExpired)
+                        {
+                            //address found after yours on the way to the destination
+                            WriteListBox($"[{ttl}] - Route: {reply.Address} - Time: {s1.ElapsedMilliseconds} ms - Total Time: {s2.ElapsedMilliseconds} ms");
+                            continue; //continue to the other bits to find more servers
+                        }
+                        if (reply.Status == IPStatus.TimedOut)
+                        {
+                            //this would occour if it takes too long for the server to reply or if a server has the ICMP port closed (quite common for this).
+                            WriteListBox($"Timeout on {hostname}. Continuing.");
+                            continue;
+                        }
+                        if (reply.Status == IPStatus.Success)
+                        {
+                            //the ICMP packet has reached the destination (the hostname)
+                            WriteListBox($"Successful Trace route to {hostname} in {s1.ElapsedMilliseconds} ms - Total Time: {s2.ElapsedMilliseconds} ms");
+                            s1.Stop();
+                            s2.Stop();
+                        }
+                    }
+                    break;
+                }
+            }));
+        }
+
+        private void WriteListBox(String text)
+        {
+            Console.WriteLine(text);
+            listBox1.Items.Add(text);
+        }
     }
 
   
